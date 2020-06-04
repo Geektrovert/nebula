9716c4f9eb55af997f5ccd730f152c89df8c079c
Samnan <namanush.rsr.16@gmail.com>
2020-06-04 19:42:32 +0600

    update next config. remove unused deps

--- next.config.js
+++ next.config.js
@@ -1,21 +1,30 @@
-const withSass = require('@zeit/next-sass');
-const withCSS = require('@zeit/next-css');
-const withTM = require('next-transpile-modules');
+const withSass = require("@zeit/next-sass");
+const withCSS = require("@zeit/next-css");
+const withTM = require("next-transpile-modules");
 
 module.exports = withCSS(
-	withSass(
-		withTM({
-  		transpileModules: [
-  			'react-flexbox-grid',
-  			'react-syntax-highlighter',
-  		],
-  		webpack: function(config) {
-		    config.module.rules.push({
-		      test: /\.md$/,
-		      use: 'raw-loader',
-		    })
-		    return config
-		  },
-		})
-	)
+  withSass(
+    withTM({
+      transpileModules: ["react-syntax-highlighter"],
+      webpack: function (config) {
+        config.module.rules.push({
+          test: /\.md$/,
+          use: "raw-loader",
+        });
+        return config;
+      },
+      exportPathMap: async function (
+        defaultPathMap,
+        { dev, dir, outDir, distDir, buildId }
+      ) {
+        return {
+          "/": { page: "/" },
+          "/writings/guide": {
+            page: "/writings/[slug]",
+            query: { slug: "guide" },
+          },
+        };
+      },
+    })
+  )
 );
