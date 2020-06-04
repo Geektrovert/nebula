55a27e3c0e08d08246edbe844278600fafb22a61
Samnan <namanush.rsr.16@gmail.com>
2020-06-05 05:03:00 +0600

    update og:image

--- pages/_app.js
+++ pages/_app.js
@@ -13,7 +13,7 @@ function MyApp({ Component, pageProps }) {
         <meta property="og:title" content={title || `Nebula`} />
         <meta property="og:site_name" content="Nebula" />
         <meta property="og:description" content={og ? og.description : `A modern next.js blog theme`} />
-        <meta property="og:image" content={og ? og.image : `https://nebula-blog.netlify.app/og/default.svg`} />
+        <meta property="og:image" content={og ? og.image : `/og/default.png`} />
 
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono&family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" />
         <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
