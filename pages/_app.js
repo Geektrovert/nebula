6026f684c9ae5774f291984070d63b914f096c8b
Samnan <namanush.rsr.16@gmail.com>
2020-06-07 03:47:21 +0600

    fix warning

--- pages/_app.js
+++ pages/_app.js
@@ -16,7 +16,7 @@ function MyApp({ Component, pageProps }) {
         <meta property="og:image" content={og ? og.image : `/og/default.png`} />
 
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono&family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" />
-        <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
 
         <title>{title || `Nebula`}</title>
       </Head>
